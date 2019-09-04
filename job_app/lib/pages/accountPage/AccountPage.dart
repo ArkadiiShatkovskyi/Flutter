733cca57c861f94440fff795cd21d4b0861ecae6
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-09-05 00:43:36 +0200

    Account Page first UI was created

---
+++ job_app/lib/pages/accountPage/AccountPage.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:job_app/items/StyleSettings.dart';
+import 'package:job_app/pages/mainPage/MainPage.dart';
+import 'package:ant_icons/ant_icons.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:job_app/pages/authorizationPage/Authorization.dart';
+import './DataElement.dart';
+
+class AccountPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AccountPageState();
+  }
+}
+
+class _AccountPageState extends State<AccountPage> {
+  var _name = '';
+  var _surname = '';
+  var _birthDate = '';
+  var _email = '';
+
+  @override
+  void initState() {
+    super.initState();
+    Authorization _authorization = Authorization();
+    _authorization.getUser().then((currUser) {
+      _email = currUser.email;
+      _getPersonalData(currUser.uid);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text("Account page"),
+            backgroundColor: styleColor,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (context) => MainPage())),
+            )),
+        body: _createBody(),
+      ),
+    );
+  }
+
+  Widget _createBody() {
+    return Column(
+      children: <Widget>[
+        Container(
+            padding: EdgeInsets.only(top: 30),
+            width: double.infinity,
+            child: Icon(
+              AntIcons.user,
+              size: 100,
+              color: styleColor,
+            )),
+        Divider(
+          height: 15,
+          color: styleColor,
+        ),
+        DataElement("Name", _name.toString()),
+        Divider(
+          height: 15,
+          color: styleColor,
+        ),
+        DataElement("Surname", _surname.toString()),
+        Divider(
+          height: 15,
+          color: styleColor,
+        ),
+        DataElement("Birth Day", _birthDate.toString()),
+        Divider(
+          height: 15,
+          color: styleColor,
+        ),
+        DataElement("Email", _email.toString()),
+      ],
+    );
+  }
+
+  void _getPersonalData(String user) async {
+    var document = await Firestore.instance
+        .collection('personalData')
+        .document(user)
+        .get();
+    setState(() {
+      _name = document['name'];
+      _surname = document['surname'];
+      _birthDate = document['birthDay'];
+    });
+  }
+}
