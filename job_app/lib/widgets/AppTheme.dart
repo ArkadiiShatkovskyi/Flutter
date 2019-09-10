806a5a286f5a14c1ec8d7dd7dbc7d97535180c4f
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-09-11 00:26:40 +0200

    Adding new settings to theme

--- job_app/lib/widgets/AppTheme.dart
+++ job_app/lib/widgets/AppTheme.dart
@@ -14,6 +14,7 @@ ThemeData get appTheme {
     accentColor: styleColor,
 //    dividerColor: styleColor,
     buttonColor: styleColor,
+    inputDecorationTheme: InputDecorationTheme(focusColor: styleColor),
     iconTheme: IconThemeData(color: styleColor),
     textTheme: TextTheme(
       title: TextStyle(
@@ -29,35 +30,3 @@ ThemeData get appTheme {
     ),
   );
 }
-
-/*mixin AppTheme implements ThemeData {
-  Color get styleColor {
-    return _hexToColor("#7d93fe");
-  }
-
-  Color _hexToColor(String code) {
-    return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
-  }
-
-  ThemeData get theme {
-    return ThemeData(
-        primaryColor: this.styleColor,
-        dividerColor: this.styleColor,
-        buttonColor: this.styleColor,
-        iconTheme: IconThemeData(color: this.styleColor),
-        textTheme: TextTheme(
-          title: TextStyle(
-            fontFamily: 'CourgetteRegular',
-            fontSize: 20,
-          ),
-        ),
-        appBarTheme: AppBarTheme(
-          color: this.styleColor,
-          textTheme: ThemeData.light().textTheme.copyWith(
-                  title:
-                      TextStyle(fontSize: 20, fontFamily: 'CourgetteRegular'),
-                ),
-          ),
-        );
-  }
-}*/
