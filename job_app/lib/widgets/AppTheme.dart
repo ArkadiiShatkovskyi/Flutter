8b39fef1391fa4c6abb57605aa0d3104e6d8dd47
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-09-10 22:58:06 +0200

    Set up a theme

--- job_app/lib/widgets/AppTheme.dart
+++ job_app/lib/widgets/AppTheme.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
 
-mixin AppTheme implements ThemeData {
+Color get styleColor {
+  return _hexToColor("#7d93fe");
+}
+
+Color _hexToColor(String code) {
+  return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
+}
+
+ThemeData get appTheme {
+  return ThemeData(
+    primaryColor: styleColor,
+    dividerColor: styleColor,
+    buttonColor: styleColor,
+    iconTheme: IconThemeData(color: styleColor),
+    textTheme: TextTheme(
+      title: TextStyle(
+        fontFamily: 'CourgetteRegular',
+        fontSize: 20,
+      ),
+    ),
+    appBarTheme: AppBarTheme(
+      color: styleColor,
+      textTheme: ThemeData.light().textTheme.copyWith(
+            title: TextStyle(fontSize: 20, fontFamily: 'CourgetteRegular'),
+          ),
+    ),
+  );
+}
+
+/*mixin AppTheme implements ThemeData {
   Color get styleColor {
     return _hexToColor("#7d93fe");
   }
@@ -30,4 +59,4 @@ mixin AppTheme implements ThemeData {
           ),
         );
   }
-}
+}*/
