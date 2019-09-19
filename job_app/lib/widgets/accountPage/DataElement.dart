475a0c32960f1764589b94d4feb841cfba0d5708
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-09-19 20:58:32 +0200

    Responsive UI

--- job_app/lib/widgets/accountPage/DataElement.dart
+++ job_app/lib/widgets/accountPage/DataElement.dart
@@ -8,24 +8,25 @@ class DataElement extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final media = MediaQuery.of(context);
     return Container(
       width: double.infinity,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Container(
-            width: 200,
+            width: media.size.width * .5,
             alignment: Alignment.centerLeft,
             child: Text(
               _text,
               style: TextStyle(fontWeight: FontWeight.w400, fontSize: 17),
             ),
-            padding: EdgeInsets.only(left: 75, right: 15, top: 15, bottom: 15),
+            padding: EdgeInsets.only(left: media.size.width * .15, top: media.size.height * .01, bottom: media.size.height * .01),
           ),
           Container(
             alignment: Alignment.centerLeft,
-            width: 200,
-            padding: EdgeInsets.only(top: 15, bottom: 15),
+            width: media.size.width * .5,
+            padding: EdgeInsets.only(top: media.size.height * .01, bottom: media.size.height * .01),
             child: Text(_dataText),
           ),
         ],
