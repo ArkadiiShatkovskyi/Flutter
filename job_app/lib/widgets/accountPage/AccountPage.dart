418ad575aef6ff00db7235be33b655d0de2ac768
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-09-19 20:57:49 +0200

    Testing page

--- job_app/lib/widgets/accountPage/AccountPage.dart
+++ job_app/lib/widgets/accountPage/AccountPage.dart
@@ -33,7 +33,21 @@ class _AccountPageState extends State<AccountPage> {
 
   @override
   Widget build(BuildContext context) {
+    final media = MediaQuery.of(context);
     return MaterialApp(
+        theme: appTheme,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+                title: Text("Account page"),
+                leading: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () => Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => MainPage())),
+                )),
+            body: Container(
+                child: Center(child: Text("Page is on the testing")))));
+    /*MaterialApp(
       theme: appTheme,
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -44,39 +58,35 @@ class _AccountPageState extends State<AccountPage> {
               onPressed: () => Navigator.pushReplacement(
                   context, MaterialPageRoute(builder: (context) => MainPage())),
             )),
-        body: _createBody(),
+        body: Column(
+          children: <Widget>[
+            Container(
+                padding: EdgeInsets.only(top: media.size.height * .05),
+                width: double.infinity,
+                child: Icon(
+                  AntIcons.user,
+                  size: media.size.height * .2,
+                )),
+            Divider(
+              height: media.size.height * .02,
+            ),
+            DataElement("Name", _name.toString()),
+            Divider(
+              height: 15,
+            ),
+            DataElement("Surname", _surname.toString()),
+            Divider(
+              height: 15,
+            ),
+            DataElement("Birth Day", _birthDate.toString()),
+            Divider(
+              height: 15,
+            ),
+            DataElement("Email", _email.toString()),
+          ],
+        ),
       ),
-    );
-  }
-
-  Widget _createBody() {
-    return Column(
-      children: <Widget>[
-        Container(
-            padding: EdgeInsets.only(top: 30),
-            width: double.infinity,
-            child: Icon(
-              AntIcons.user,
-              size: 100,
-            )),
-        Divider(
-          height: 15,
-        ),
-        DataElement("Name", _name.toString()),
-        Divider(
-          height: 15,
-        ),
-        DataElement("Surname", _surname.toString()),
-        Divider(
-          height: 15,
-        ),
-        DataElement("Birth Day", _birthDate.toString()),
-        Divider(
-          height: 15,
-        ),
-        DataElement("Email", _email.toString()),
-      ],
-    );
+    );*/
   }
 
   void _getPersonalData(String user) async {
