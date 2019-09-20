997b505633607b7b0f25951ad9ee4ddcae587d55
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-09-21 00:39:21 +0200

    Adding document id to class

--- job_app/lib/widgets/mainPage/tabs/dataTab/Record.dart
+++ job_app/lib/widgets/mainPage/tabs/dataTab/Record.dart
@@ -1,10 +1,11 @@
 class Record {
 
+  final id;
   final date;
   final startTime;
   final endTime;
   final workTime;
   final rate;
 
-  const Record({this.date, this.startTime, this.endTime, this.workTime, this.rate});
+  const Record({this.id, this.date, this.startTime, this.endTime, this.workTime, this.rate});
 }
