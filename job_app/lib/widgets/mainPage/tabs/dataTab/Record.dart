a79f6eb8ddd8bec14d53ebb51ee499a4c6142657
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-09-20 22:16:45 +0200

    Creating class whitch is representation of row

---
+++ job_app/lib/widgets/mainPage/tabs/dataTab/Record.dart
@@ -0,0 +1,10 @@
+class Record {
+
+  final date;
+  final startTime;
+  final endTime;
+  final workTime;
+  final rate;
+
+  const Record({this.date, this.startTime, this.endTime, this.workTime, this.rate});
+}
