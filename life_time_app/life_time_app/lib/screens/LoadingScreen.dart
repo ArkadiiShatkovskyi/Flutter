0cb65c585a0dc0840774c4b8cb8d67071b6f80af
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-10-08 02:04:37 +0200

    Main screen was created

--- life_time_app/life_time_app/lib/screens/LoadingScreen.dart
+++ life_time_app/life_time_app/lib/screens/LoadingScreen.dart
@@ -3,13 +3,29 @@ import 'package:flutter/material.dart';
 
 import 'package:flare_flutter/flare_actor.dart';
 
-class LoadingScreen extends StatelessWidget{
+import './TimeScreen.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  State createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => TimeScreen()));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GestureDetector(
         child: FlareActor(
-          'assets/animation/ActivityAnimation.flr',
+          'assets/animation/LoadingAnimation.flr',
           fit: BoxFit.cover,
           animation: 'activity',
         ),
