4a8725d3b9624a9d76fa994f3179b62fa8b5e883
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-10-08 02:58:42 +0200

    Time screen UI design was created

--- life_time_app/life_time_app/lib/screens/TimeScreen.dart
+++ life_time_app/life_time_app/lib/screens/TimeScreen.dart
@@ -1,11 +1,154 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class TimeScreen extends StatelessWidget {
+  final date = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
+    final media = MediaQuery.of(context).size;
     return Scaffold(
-      body: Text("Here's main screen"),
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+              top: media.height / 10,
+              left: media.width / 4,
+              right: media.width / 4,
+              child: Column(
+                children: <Widget>[
+                  Center(
+                      child: Text(
+                    "Your time left",
+                    style: TextStyle(fontSize: 24),
+                  )),
+                  Center(
+                      child: Text(
+                    "Date ${date.day}:${date.month}:${date.year}",
+                    style: TextStyle(fontSize: 18),
+                  )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Icon(
+                    Icons.access_time,
+                    size: 150,
+                  ),
+                ],
+              )),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: media.width,
+              height: media.height / 2,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.lightBlueAccent,
+                    Colors.blueAccent,
+                  ],
+                ),
+//                color: Colors.black54,
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    topLeft: Radius.circular(30)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 30.0, bottom: 30),
+                child: ListView(
+                  children: <Widget>[
+                    SizedBox(
+                      height: media.height * .2,
+                      child: Card(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                        margin: EdgeInsets.only(top: 30, left: 40, right: 40),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Center(
+                                child: Text(
+                              "Title of the card",
+                              style: TextStyle(fontSize: 20),
+                            )),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Text("some text"),
+                                Text("123 4124 412"),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+//                      width: media.width * .9,
+                      height: media.height * .2,
+                      child: Card(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                        margin: EdgeInsets.only(top: 30, left: 40, right: 40),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Center(
+                                child: Text(
+                              "Title of the card",
+                              style: TextStyle(fontSize: 20),
+                            )),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Text("some text"),
+                                Text("123 4124 412"),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+//                      width: media.width * .9,
+                      height: media.height * .2,
+                      child: Card(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                        margin: EdgeInsets.only(top: 30, left: 40, right: 40),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Center(
+                                child: Text(
+                              "Title of the card",
+                              style: TextStyle(fontSize: 20),
+                            )),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Text("some text"),
+                                Text("123 4124 412"),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
