83e9cf3c1e7dc4967541bd8fd14bbf22df1c8db9
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-10-15 01:42:16 +0200

    Changing list UI

--- life_time_app/life_time_app/lib/screens/TimeScreen.dart
+++ life_time_app/life_time_app/lib/screens/TimeScreen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
+import '../widgets/StatisticItem.dart';
+
 class TimeScreen extends StatelessWidget {
   final date = DateTime.now();
 
@@ -9,7 +11,7 @@ class TimeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final media = MediaQuery.of(context).size;
     return Scaffold(
-     /* bottomNavigationBar: BottomNavigationBar(
+      /* bottomNavigationBar: BottomNavigationBar(
         elevation: 0,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.add), title: Text("Add")),
@@ -20,168 +22,74 @@ class TimeScreen extends StatelessWidget {
         width: media.width,
         height: media.height,
         child: Stack(
-            children: <Widget>[
-              Positioned(
-                top: 25,
-                right: media.width - 60,
-                child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      shape: BoxShape.circle,
+          children: <Widget>[
+            Positioned(
+              top: 10,
+              right: media.width - 60,
+              child: Container(
+                  decoration: BoxDecoration(
+//                      color: Colors.grey,
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.menu,
+                      color: Colors.black,
                     ),
-                    child: IconButton(icon: Icon(Icons.menu, color: Colors.white,), color: Colors.white,)),
-              ),
-              Positioned(
-                  top: media.height / 10,
-                  left: media.width / 4,
-                  right: media.width / 4,
-                  child: Column(
+                    color: Colors.white,
+                  )),
+            ),
+            Positioned(
+                top: media.height / 10,
+                left: media.width / 4,
+                right: media.width / 4,
+                child: Column(
+                  children: <Widget>[
+                    Center(
+                        child: Text(
+                      "Your time left",
+                      style: TextStyle(fontSize: 24),
+                    )),
+                    Center(
+                        child: Text(
+                      "Date ${date.day}:${date.month}:${date.year}",
+                      style: TextStyle(fontSize: 18),
+                    )),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Icon(
+                      Icons.access_time,
+                      size: 150,
+                    ),
+                  ],
+                )),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                width: media.width,
+                height: media.height * .55,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      topLeft: Radius.circular(30)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 30, bottom: 0),
+                  child: ListView(
+                    physics: NeverScrollableScrollPhysics(),
                     children: <Widget>[
-                      Center(
-                          child: Text(
-                        "Your time left",
-                        style: TextStyle(fontSize: 24),
-                      )),
-                      Center(
-                          child: Text(
-                        "Date ${date.day}:${date.month}:${date.year}",
-                        style: TextStyle(fontSize: 18),
-                      )),
                       SizedBox(
                         height: 10,
                       ),
-                      Icon(
-                        Icons.access_time,
-                        size: 150,
-                      ),
+                      StatisticItem(media.height * .5, 1),
+                      /***  HERE WRITE */
                     ],
-                  )),
-              Positioned(
-                bottom: 0,
-                child: Container(
-                  width: media.width,
-                  height: media.height * .55,
-                  decoration: BoxDecoration(
-                    /*gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomLeft,
-                      colors: [
-                        Colors.lightBlueAccent,
-                        Colors.blueAccent,
-                      ],
-                    ),*/
-//                color: Colors.black54,
-//                border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(30),
-                        topLeft: Radius.circular(30)),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 30, bottom: 0),
-                    child: ListView(
-                      physics: NeverScrollableScrollPhysics(),
-                      children: <Widget>[
-                        SizedBox(
-                          height: 10,
-                        ),
-                        SizedBox(
-                          height: media.height * .2,
-                          child: Card(
-                            elevation: 5,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30)),
-                            margin:
-                                EdgeInsets.only(left: 40, right: 40, bottom: 30),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Center(
-                                    child: Text(
-                                  "Title of the card",
-                                  style: TextStyle(fontSize: 20),
-                                )),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                  children: <Widget>[
-                                    Text("some text"),
-                                    Text("123 4124 412"),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-//                      width: media.width * .9,
-                          height: media.height * .2,
-                          child: Card(
-                            elevation: 5,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30)),
-                            margin:
-                                EdgeInsets.only(left: 40, right: 40, bottom: 30),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Center(
-                                    child: Text(
-                                  "Title of the card",
-                                  style: TextStyle(fontSize: 20),
-                                )),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                  children: <Widget>[
-                                    Text("some text"),
-                                    Text("123 4124 412"),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-//                      width: media.width * .9,
-                          height: media.height * .2,
-                          child: Card(
-                            elevation: 5,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30)),
-                            margin:
-                                EdgeInsets.only(left: 40, right: 40, bottom: 30),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Center(
-                                    child: Text(
-                                  "Title of the card",
-                                  style: TextStyle(fontSize: 20),
-                                )),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                  children: <Widget>[
-                                    Text("some text"),
-                                    Text("123 4124 412"),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
                   ),
                 ),
               ),
-            ],
+            ),
+          ],
         ),
       ),
     );
---
+++ life_time_app/life_time_app/lib/widgets/StatisticItem.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StatisticsList extends StatelessWidget {
+  final height;
+  final item;
+
+  const StatisticsList(this.height, this.item);
+
+  List get statistics {
+    return [
+      {'title': "This is CARD 1", 'text': '12321412412'},
+      {'title': "This is CARD 2", 'text': '12321412412'},
+      {'title': "This is CARD 3", 'text': '12321412412'}
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      child: Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        margin: EdgeInsets.only(left: 40, right: 40, bottom: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+                child: Text(
+              statistics[item]['title'],
+              style: TextStyle(fontSize: 20),
+            )),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text("some text"),
+                Text( statistics[item]['text']),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
