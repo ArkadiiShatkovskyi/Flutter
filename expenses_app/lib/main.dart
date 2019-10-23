efebdc1d0ca9b1af7169d4d4216069f418213e84
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-10-23 20:48:25 +0200

    Changing main screen file

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -2,8 +2,5 @@ import 'package:expenses_app/screens/ExpensesScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: ExpensesScreen(),
-      ),
+      ExpensesScreen(),
     );
