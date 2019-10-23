586da9f16d652c4cc3a620e090498cd5ef1801a1
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-10-23 20:49:58 +0200

    Changing expenses screen view

--- expenses_app/lib/screens/ExpensesScreen.dart
+++ expenses_app/lib/screens/ExpensesScreen.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/ExpensesList.dart';
+import '../widgets/CategoriesWidget.dart';
 
 class ExpensesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "Your expenses",
-          style: TextStyle(
-            fontSize: 22,
-            fontStyle: FontStyle.italic,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Expenses",
+            style: TextStyle(
+              fontSize: 22,
+              fontStyle: FontStyle.italic,
+            ),
           ),
         ),
+        body: Column(children: <Widget>[
+            CategoriesWidget(),
+//        ExpensesList(5),
+        ]),
       ),
-      body: ExpensesList(5),
     );
   }
 }
