3518809438835ffeaffcedfaaef5deb424d17406
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-10-24 02:29:12 +0200

    Creating list of expenses

--- expenses_app/lib/widgets/ExpensesList.dart
+++ expenses_app/lib/widgets/ExpensesList.dart
@@ -1,29 +1,107 @@
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatefulWidget {
-
-  final itemCount;
-
-  ExpensesList(this.itemCount);
-
   @override
-  State<StatefulWidget> createState() => ExpensesListState();
+  State<StatefulWidget> createState() => _ExpensesListState();
 }
 
-class ExpensesListState extends State<ExpensesList> {
+class _ExpensesListState extends State<ExpensesList> {
+  var expensesList = [
+    {
+      'title': 'First expenses',
+      'money': 0,
+      'date': (DateTime.now().day.toString() +
+          '.' +
+          DateTime.now().month.toString())
+    },
+    {
+      'title': 'Second expenses',
+      'money': 0,
+      'date': (DateTime.now().day.toString() +
+          '.' +
+          DateTime.now().month.toString())
+    },
+    {
+      'title': 'Third expenses',
+      'money': 0,
+      'date': (DateTime.now().day.toString() +
+          '.' +
+          DateTime.now().month.toString())
+    },
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: widget.itemCount,
-        itemBuilder: (ctx, i){
-        return Row(
-          children: <Widget>[
-            Icon(Icons.attach_money,),
-            Padding(
-                padding: EdgeInsets.only(left: 10),
-                child: Text("Expenses title")),
-          ],
-        );
-    });
+    final media = MediaQuery.of(context);
+    return SizedBox(
+      height: media.size.height * .8,
+      child: ListView.builder(
+//      physics: NeverScrollableScrollPhysics(),
+          itemCount: expensesList.length,
+          itemBuilder: (ctx, i) {
+            return getItem(i);
+          }),
+    );
+  }
+
+  Widget getItem(int index) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 50, bottom: 10, right: 50),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Icon(
+            Icons.info_outline,
+            color: Colors.indigoAccent,
+            size: 30,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            expensesList[index]['title'],
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            expensesList[index]['money'].toString(),
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              fontSize: 18,
+            ),
+          ),
+          Icon(
+            Icons.attach_money,
+            color: Colors.indigoAccent,
+            size: 22,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            expensesList[index]['date'],
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              fontSize: 18,
+            ),
+          ),
+          Icon(
+            Icons.date_range,
+            color: Colors.indigoAccent,
+            size: 22,
+          ),
+        ],
+      ),
+    );
   }
 }
