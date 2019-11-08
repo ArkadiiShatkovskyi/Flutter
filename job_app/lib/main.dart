77e75be54d3989517927625413b8e563f702d6c1
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2019-11-08 12:19:58 +0100

    Recreating sign in sign up pages

--- job_app/lib/main.dart
+++ job_app/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 import './widgets/authorizationPage/SignInSignUpPage.dart';
+import './widgets/authorizationPage/SignInPage.dart';
 
 void main() => runApp(JobApp());
 
 class JobApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return SignInSignUp();
+    return SingInPage();
   }
 }
