cf17eecf3971607ec7eb61715b4bf71979fc196a
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2019-11-11 23:23:50 +0100

    Image asset was added

--- job_app/ios/Flutter/flutter_export_environment.sh
+++ job_app/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/arek/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/arek/Development/dev_folder/job_app"
+export "FLUTTER_APPLICATION_PATH=/home/arek/Development/Flutter/job_app"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
