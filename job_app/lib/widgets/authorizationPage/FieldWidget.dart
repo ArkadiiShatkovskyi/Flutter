737feb31be49be801f5291a45465d32526479960
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2019-11-11 23:41:24 +0100

    Creating field widget

---
+++ job_app/lib/widgets/authorizationPage/FieldWidget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class FieldWidget extends StatelessWidget {
+  final width;
+  final textController;
+  final icon;
+  final labelText;
+
+  FieldWidget({this.width, this.textController, this.icon, this.labelText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: width * .1, right: width * .1),
+      child: TextFormField(
+        maxLines: 1,
+        maxLength: 30,
+        obscureText: false,
+        decoration: InputDecoration(
+          icon: Icon(
+            icon,
+            color: Theme.of(context).primaryColor,
+          ),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+          labelText: labelText,
+        ),
+        controller: textController,
+      ),
+    );
+  }
+}
