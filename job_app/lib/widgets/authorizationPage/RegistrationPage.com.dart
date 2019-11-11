2134c1d77b7cbd71189909f1b8e8f56f588828f6
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2019-11-11 22:48:01 +0100

    Creating first UI of registration page

--- job_app/lib/widgets/authorizationPage/RegistrationPage.com.dart
+++ job_app/lib/widgets/authorizationPage/RegistrationPage.com.dart
@@ -5,114 +5,165 @@ import 'package:ant_icons/ant_icons.dart';
 
 import 'package:job_app/widgets/AppTheme.dart';
 
-class RegistrationPage extends StatelessWidget {
-  final navigatorKey = GlobalKey<NavigatorState>();
-
+class RegistrationPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: appTheme,
-        navigatorKey: navigatorKey,
-        debugShowCheckedModeBanner: false,
-        home: WillPopScope(
-            onWillPop: () async => false,
-            child: Scaffold(
-                appBar: AppBar(
-                  title: Text("Create new account"),
-                ),
-                body: RegistrationPageBody(navigatorKey))));
-  }
+  State createState() => _RegistrationPageState();
 }
 
-class RegistrationPageBody extends StatefulWidget {
-  RegistrationPageBody(GlobalKey key);
-
-  @override
-  State createState() => _RegistrationPageBodyState();
-}
-
-class _RegistrationPageBodyState extends State<RegistrationPageBody> {
+class _RegistrationPageState extends State<RegistrationPage> {
   final _emailTextController = TextEditingController();
   final _passwordTextController = TextEditingController();
+  final _nameTextController = TextEditingController();
+  final _surNameTextController = TextEditingController();
+
 //  Authorization _db = Authorization();
 
   Widget build(BuildContext context) {
     final media = MediaQuery.of(context);
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.only(top: 40),
-        child: ListView(
-          children: <Widget>[
-            Container(
-              child: Center(
-                child: Text(
-                  "Create an account",
-                  style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(
-                  left: media.size.width * .1, right: media.size.width * .1),
-              child: TextFormField(
-                maxLines: 1,
-                maxLength: 30,
-                obscureText: false,
-                decoration: InputDecoration(
-                  icon: Icon(
-                    AntIcons.mail_outline,
-                    color: Theme.of(context).primaryColor,
+    return MaterialApp(
+      theme: appTheme,
+      debugShowCheckedModeBanner: false,
+      home: WillPopScope(
+        onWillPop: () async => false,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Create new account"),
+          ),
+          body: Center(
+            child: Container(
+              margin: const EdgeInsets.only(top: 40),
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    child: Center(
+                      child: Text(
+                        "Fields bellow",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontStyle: FontStyle.italic,
+                            color: Theme.of(context).primaryColor),
+                      ),
+                    ),
                   ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  labelText: 'Email',
-                ),
-                controller: _emailTextController,
-              ),
-            ),
-            Divider(
-              color: Colors.transparent,
-              height: media.size.height * .01,
-            ),
-            Container(
-              margin: EdgeInsets.only(
-                  left: media.size.width * .1, right: media.size.width * .1),
-              child: TextFormField(
-                maxLines: 1,
-                maxLength: 20,
-                obscureText: true,
-                decoration: InputDecoration(
-                  icon: Icon(
-                    AntIcons.lock_outline,
-                    color: Theme.of(context).primaryColor,
+                  SizedBox(
+                    height: 30,
                   ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  labelText: 'Password',
-                ),
-                controller: _passwordTextController,
+                  Container(
+                    margin: EdgeInsets.only(
+                        left: media.size.width * .1,
+                        right: media.size.width * .1),
+                    child: TextFormField(
+                      maxLines: 1,
+                      maxLength: 20,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          AntIcons.user,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        labelText: 'Name',
+                      ),
+                      controller: _nameTextController,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.transparent,
+                    height: media.size.height * .01,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                        left: media.size.width * .1,
+                        right: media.size.width * .1),
+                    child: TextFormField(
+                      maxLines: 1,
+                      maxLength: 30,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          AntIcons.user,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        labelText: 'Surname',
+                      ),
+                      controller: _surNameTextController,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.transparent,
+                    height: media.size.height * .01,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                        left: media.size.width * .1,
+                        right: media.size.width * .1),
+                    child: TextFormField(
+                      maxLines: 1,
+                      maxLength: 30,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          AntIcons.mail_outline,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        labelText: 'Email',
+                      ),
+                      controller: _emailTextController,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.transparent,
+                    height: media.size.height * .01,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                        left: media.size.width * .1,
+                        right: media.size.width * .1),
+                    child: TextFormField(
+                      maxLines: 1,
+                      maxLength: 20,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          AntIcons.lock_outline,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        labelText: 'Password',
+                      ),
+                      controller: _passwordTextController,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.transparent,
+                    height: media.size.height * .01,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                        left: media.size.width * .25,
+                        right: media.size.width * .25),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      padding: const EdgeInsets.all(20.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      onPressed: () => {},
+                      child: Text(
+                        "Create an account",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Divider(
-              color: Colors.transparent,
-              height: media.size.height * .01,
-            ),
-            Container(
-              margin: EdgeInsets.only(
-                  left: media.size.width * .25, right: media.size.width * .25),
-              child: RaisedButton(
-                textColor: Colors.white,
-                padding: const EdgeInsets.all(20.0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0)),
-                onPressed: () => {},
-                child: Text(
-                  "Create an account",
-                  style: TextStyle(fontSize: 15),
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
