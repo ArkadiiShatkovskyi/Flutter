ad34f6812537572cc5cafa7fc02c38acf6d4e3ed
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2019-11-12 05:21:57 +0100

    Page view ui was created

--- job_app/lib/widgets/authorizationPage/RegistrationPage.com.dart
+++ job_app/lib/widgets/authorizationPage/RegistrationPage.com.dart
@@ -2,10 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:ant_icons/ant_icons.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:job_app/widgets/AppTheme.dart';
-import './FieldWidget.dart';
 import './RegistrationStep.dart';
 
 class RegistrationPage extends StatefulWidget {
@@ -19,30 +17,34 @@ class _RegistrationPageState extends State<RegistrationPage> {
   final _nameTextController = TextEditingController();
   final _surNameTextController = TextEditingController();
   final _pageViewController = PageController();
+  var _registrationSteps;
 
-  var _registrationSteps = <Widget>[
-    RegistrationStep(
-      image: "assets/images/undraw_personal_data_29co.svg",
-      textController1: _nameTextController,
-      icon1: AntIcons.user,
-      labelText1: 'Name',
-      textController2: _surNameTextController,
-      icon2: AntIcons.user,
-      labelText2: 'Surname',
-    ),
-    RegistrationStep(
-      image: "assets/images/undraw_personal_data_29co.svg",
-      textController1: _emailTextController,
-      icon1: AntIcons.mail_outline,
-      labelText1: 'Email',
-      textController2: _passwordTextController,
-      icon2: AntIcons.lock_outline,
-      labelText2: 'Password',
-    )
-  ];
+  @override
+  void initState() {
+    super.initState();
+    _registrationSteps = <Widget>[
+      RegistrationStep(
+        image: "assets/images/undraw_personal_data_29co.svg",
+        textController1: _nameTextController,
+        icon1: AntIcons.user,
+        labelText1: 'Name',
+        textController2: _surNameTextController,
+        icon2: AntIcons.user,
+        labelText2: 'Surname',
+      ),
+      RegistrationStep(
+        image: "assets/images/undraw_messaging_app_ch6t.svg",
+        textController1: _emailTextController,
+        icon1: AntIcons.mail_outline,
+        labelText1: 'Email',
+        textController2: _passwordTextController,
+        icon2: AntIcons.lock_outline,
+        labelText2: 'Password',
+      )
+    ];
+  }
 
   Widget build(BuildContext context) {
-    final media = MediaQuery.of(context);
     return MaterialApp(
       theme: appTheme,
       debugShowCheckedModeBanner: false,
@@ -54,47 +56,14 @@ class _RegistrationPageState extends State<RegistrationPage> {
               AntIcons.arrow_right,
               color: Colors.white,
             ),
-            onPressed: () => {},
+            onPressed: nextPage,
           ),
           floatingActionButtonLocation:
               FloatingActionButtonLocation.centerFloat,
           appBar: AppBar(
             title: Text("Create new account"),
           ),
-          body: Center(
-            child: Container(
-              margin: const EdgeInsets.only(top: 40),
-              child: ListView(
-                children: <Widget>[
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    width: media.size.width * .5,
-                    height: media.size.height * .3,
-                    margin: const EdgeInsets.only(bottom: 20),
-                    child: SvgPicture.asset(
-                        "assets/images/undraw_personal_data_29co.svg"),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  FieldWidget(
-                      width: media.size.width,
-                      icon: AntIcons.user,
-                      textController: _nameTextController,
-                      labelText: 'Name'),
-                  SizedBox(height: 10),
-                  FieldWidget(
-                      width: media.size.width,
-                      icon: AntIcons.user,
-                      textController: _surNameTextController,
-                      labelText: 'Surname'),
-                  SizedBox(height: 10),
-                ],
-              ),
-            ),
-          ),
+          body: createPageView(),
         ),
       ),
     );
@@ -104,7 +73,15 @@ class _RegistrationPageState extends State<RegistrationPage> {
     return PageView(
       controller: _pageViewController,
       physics: NeverScrollableScrollPhysics(),
-      children: <Widget>[],
+      children: _registrationSteps,
     );
+  }
+
+  void nextPage(){
+    if(_pageViewController.page == 1.0){
+      print("register here");
+    }else{
+      _pageViewController.nextPage(duration: Duration(milliseconds: 300), curve: Curves.ease);
+    }
   }
 }
