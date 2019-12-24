3a29145ee5df80c39dba9a336fb2076270b07f4e
Arkadii <arkadii.shatkovskyi@gmail.com>
2019-12-24 03:58:55 +0100

    File reader test

--- job_app/lib/main.dart
+++ job_app/lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 import './widgets/authorizationPage/SignInPage.dart';
+import './models/RateReader.dart';
 
 void main() => runApp(JobApp());
 
 class JobApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    writeCounter(17.0);
+    _getRate();
+//    print("Rate readed: " + result.toString());
     return SingInPage();
+  }
+
+  void _getRate() async{
+    var result =  await readCounter();
+    print("Rate readed: " + result.toString());
   }
 }
