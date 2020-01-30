884d0fee440605050cfc32a9fb0b769e92695ef4
Arkadii <arkadii.shatkovskyi@gmail.com>
2020-01-30 01:10:47 +0100

    First recipe UI was created

--- meals_app/lib/widgets/RecipeWidget.dart
+++ meals_app/lib/widgets/RecipeWidget.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 
 class RecipeWidget extends StatelessWidget {
+
+  final size;
+
+  RecipeWidget(this.size);
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: size.width * 0.8,
+      height: size.height * 0.45,
+      padding: EdgeInsets.all(15),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
+        borderRadius: BorderRadius.all(Radius.circular(30)),
       ),
-      child: Row(
-        children: <Widget>[
-          Image.asset('assets/images/rice.jpg'),
-        ],
-      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+          child: recipeImage()),
     );
   }
+}
+
+Widget recipeImage() {
+  return AspectRatio(
+    aspectRatio: 16.0 / 9.0,
+    child: Image.asset(
+      'assets/images/rice.jpg',
+      fit: BoxFit.cover,
+    ),
+  );
 }
