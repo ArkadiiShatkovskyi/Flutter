941c8c0ccabc3c9070538dadf7cfe8bddcb8cdf0
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2020-02-05 01:24:19 +0100

    Clean up file

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Meals application',
       theme: ThemeData(
         primarySwatch: Colors.amber,
       ),
