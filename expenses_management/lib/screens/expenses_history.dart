d047cad98799e913cd83fb04759972c7c6c0cd3d
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2020-02-24 21:45:10 +0100

    Test UI was created

--- expenses_management/lib/screens/expenses_history.dart
+++ expenses_management/lib/screens/expenses_history.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
-class ExpensesHistory extends StatelessWidget{
+class ExpensesHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final media = MediaQuery.of(context).size;
     return Scaffold(
-      body: Text("Expenses history"),
+      appBar: AppBar(
+        title: Text("Expenses app"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+              top: media.height * .05,
+              left: media.width * 0.3,
+              right: media.width * 0.3,
+              child: Text("Expenses history")),
+          Positioned(
+              top: 200,
+              child: Text("Expenses history")),
+        ],
+      ),
     );
   }
 }
