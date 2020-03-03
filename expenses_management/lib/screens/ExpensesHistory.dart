51797289e28c8550c1c0bf69e3b0225e29e4d20a
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2020-03-03 23:16:07 +0100

    Bottom navigation bar was added

--- expenses_management/lib/screens/ExpensesHistory.dart
+++ expenses_management/lib/screens/ExpensesHistory.dart
@@ -10,18 +10,50 @@ class ExpensesHistory extends StatelessWidget {
       appBar: AppBar(
         title: Text("Expenses app"),
       ),
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-              top: media.height * .05,
-              left: media.width * 0.3,
-              right: media.width * 0.3,
-              child: Center(child: Text("Expenses history"))),
-          Positioned(
-            top: media.height * .1,
-            child: HistoryList(),
+      bottomNavigationBar: BottomAppBar(
+        color: Theme.of(context).primaryColor,
+        shape: const CircularNotchedRectangle(),
+        child: Container(
+          height: media.height * 0.08,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: media.width * 0.05,
+              right: media.width * 0.05,
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.menu,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          /*Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddScreen()));*/
+        },
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: Container(
+        padding: EdgeInsets.only(top: media.height * .38),
+        child: HistoryList(),
       ),
     );
   }
