9b4f8f94b4753349814c0e1ade1aca87f68ba516
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2020-04-05 21:55:37 +0200

    Bloc patern for DB

---
+++ expenses_management/lib/models/ExpenseBloc.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import './DBProvider.dart';
+import './Expense.dart';
+
+class ExpenseBloc{
+  final _expenseController = StreamController<List<Expense>>.broadcast();
+  get expenses => _expenseController.stream;
+
+  ExpenseBloc(){
+    getExpenses();
+  }
+
+  getExpenses() async {
+    _expenseController.sink.add(await DBProvider.db.getExpenses());
+  }
+
+  deleteExpense(int id){
+    DBProvider.db.deleteExpense(id);
+    getExpenses();
+  }
+
+  addExpense(Expense expense){
+    DBProvider.db.newExpense(expense);
+    getExpenses();
+  }
+
+  dispose(){
+    _expenseController.close();
+  }
+}
