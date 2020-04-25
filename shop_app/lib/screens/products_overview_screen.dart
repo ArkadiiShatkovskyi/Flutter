f51d4bd789d6e3a2e874da20f6a5c064a5554cbd
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2020-04-25 17:40:27 +0200

    PopupMenu was added for filtering favorite items

--- shop_app/lib/screens/products_overview_screen.dart
+++ shop_app/lib/screens/products_overview_screen.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/products_grid.dart';
+import '../providers/products.dart';
 
-class ProductOverviewScreen extends StatelessWidget {
-  
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductOverviewScreen extends StatefulWidget {
+  @override
+  _ProductOverviewScreenState createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  bool _showOnlyFavoriteProducts = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('My Shop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  _showOnlyFavoriteProducts = true;
+                } else {
+                  _showOnlyFavoriteProducts = false;
+                }
+              });
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                  child: Text('Only Favorites'),
+                  value: FilterOptions.Favorites),
+              PopupMenuItem(
+                child: Text('Show all'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavoriteProducts),
     );
   }
 }
