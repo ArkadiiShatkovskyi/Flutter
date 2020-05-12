7707aa4982dfc19b497ea46053357e2f0d94ce5c
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2020-05-12 21:58:17 +0200

    Timer app was created

---
+++ timer_app/lib/screens/Stopwatch_Screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class StopwatchScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
+
+/* 
+
+      STOPWATCH WHERE 3 ANIMATED RINGS
+      1 - SECONDS
+      2 - MINUTES
+      3 - HOURS
+
+ */
+
+class _StopwatchScreenState extends State<StopwatchScreen>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
