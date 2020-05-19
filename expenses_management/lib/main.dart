29619926d64a20a58ec169b02c98380bca4ad2df
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2020-05-19 19:18:31 +0200

    Set up data provider

--- expenses_management/lib/main.dart
+++ expenses_management/lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
 import './screens/ExpensesPerWeekScreen.dart';
 import './screens/SignUpScreen.dart';
+
+import './models/ExpensesProvider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.indigo,
-        accentColor: Colors.orangeAccent,
+    return ChangeNotifierProvider(
+      create: (_) => ExpensesProvider(),
+      child: MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colors.indigo,
+          accentColor: Colors.orangeAccent,
+        ),
+        debugShowCheckedModeBanner: false,
+        // home: ExpensesPerWeek(),
+        home: SignUpScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-     home: ExpensesPerWeek(),
-      // home: SignUpScreen(),
     );
   }
 }
