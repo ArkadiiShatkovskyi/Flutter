9ff3a7201deaf6b29d756b8728325e144c3029bb
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2020-05-19 19:19:48 +0200

    Notations were added

--- expenses_management/lib/models/DBProvider.dart
+++ expenses_management/lib/models/DBProvider.dart
@@ -7,15 +7,15 @@ import 'package:path/path.dart';
 
 import './Expense.dart';
 
+/* Singleton class for DataBase reader */
 class DBProvider {
-  DBProvider._();
+  DBProvider._(); // private constructor
   static final DBProvider db = DBProvider._();
 
   static Database _database;
 
   Future<Database> get database async {
-    if (_database != null)
-    return _database;
+    if (_database != null) return _database;
 
     // if _database is null we instantiate it
     _database = await initDB();
@@ -25,8 +25,8 @@ class DBProvider {
   initDB() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentsDirectory.path, "ExpensesDB.db");
-    return await openDatabase(path, version: 1, onOpen: (db) {
-    }, onCreate: (Database db, int version) async {
+    return await openDatabase(path, version: 1, onOpen: (db) {},
+        onCreate: (Database db, int version) async {
       await db.execute("CREATE TABLE Expenses ("
           "id INTEGER PRIMARY KEY,"
           "title TEXT,"
@@ -41,25 +41,55 @@ class DBProvider {
 
   createTestData() async {
     final db = await database;
-    await db.insert("Expenses", Expense(id: 1, title: 'Test1', dateDay: 1, dateMonth: 3, dateYear: 2020, category: 1, amount: 14.5).toMap());
-    await db.insert("Expenses", Expense(id: 2, title: 'Test2', dateDay: 1, dateMonth: 3, dateYear: 2020, category: 1, amount: 14.5).toMap());
-    await db.insert("Expenses", Expense(id: 3, title: 'Test3', dateDay: 1, dateMonth: 3, dateYear: 2020, category: 1, amount: 14.5).toMap());
+    await db.insert(
+        "Expenses",
+        Expense(
+          id: 1,
+          title: 'Test1',
+          dateDay: 1,
+          dateMonth: 3,
+          dateYear: 2020,
+          category: "none",
+          amount: 14.5,
+        ).toMap());
+    await db.insert(
+        "Expenses",
+        Expense(
+          id: 2,
+          title: 'Test2',
+          dateDay: 1,
+          dateMonth: 3,
+          dateYear: 2020,
+          category: "none",
+          amount: 14.5,
+        ).toMap());
+    await db.insert(
+        "Expenses",
+        Expense(
+          id: 3,
+          title: 'Test3',
+          dateDay: 1,
+          dateMonth: 3,
+          dateYear: 2020,
+          category: "none",
+          amount: 14.5,
+        ).toMap());
   }
 
-  newExpense(Expense newExpense) async{
+  addNewExpense(Expense newExpense) async {
     final db = await database;
     var res = await db.insert("Expenses", newExpense.toMap());
     return res;
   }
 
-  getExpense(int id) async{
+  getExpense(int id) async {
     final db = await database;
-    var res = await db.query("Expenses", where: "id=?", whereArgs:[id]);
+    var res = await db.query("Expenses", where: "id=?", whereArgs: [id]);
     return res.isNotEmpty ? Expense.fromMap(res.first) : Null;
   }
 
   /* Geting List<Expense> of Expenses */
-  getExpenses() async{
+  getExpenses() async {
     // createTestData();
     final db = await database;
     var res = await db.query("Expenses");
@@ -68,7 +98,8 @@ class DBProvider {
 
   updateExpense(Expense newExpense) async {
     final db = await database;
-    var res = await db.update("Expenses", newExpense.toMap(), where: "id=?", whereArgs: [newExpense.id]);
+    var res = await db.update("Expenses", newExpense.toMap(),
+        where: "id=?", whereArgs: [newExpense.id]);
     return res;
   }
 
