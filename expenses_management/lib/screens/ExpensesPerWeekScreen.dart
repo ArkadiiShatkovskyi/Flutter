6fb70fb61fe1b75c513135069fb5f749f6dc6999
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2020-05-19 23:51:43 +0200

    Bug with scroll physics was fixed

--- expenses_management/lib/screens/ExpensesPerWeekScreen.dart
+++ expenses_management/lib/screens/ExpensesPerWeekScreen.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/ExpensesProvider.dart';
-import '../widgets/HistoryList.dart';
+import '../widgets/HistoryCard.dart';
 import '../widgets/ChartBarForWeek.dart';
 import './AddExpenses.dart';
 
-class ExpensesPerWeek extends StatelessWidget {
+class ExpensesPerWeek extends StatefulWidget {
+  @override
+  _ExpensesPerWeekState createState() => _ExpensesPerWeekState();
+}
+
+class _ExpensesPerWeekState extends State<ExpensesPerWeek> {
   @override
   Widget build(BuildContext context) {
     final media = MediaQuery.of(context).size;
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Expenses app"),
@@ -60,7 +64,10 @@ class ExpensesPerWeek extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           Positioned(
-            top: 0,
+            top: 0.0,
+            bottom: media.height * .39,
+            right: 0.0,
+            left: 0.0,
             child: SizedBox(
               width: media.width,
               height: media.height * .4,
@@ -76,8 +83,29 @@ class ExpensesPerWeek extends StatelessWidget {
             ),
           ),
           Positioned(
-            top: media.height * .38,
-            child: HistoryList(),
+            top: media.height * .39,
+            bottom: 0.0,
+            right: 0.0,
+            left: 0.0,
+            child: SizedBox(
+              width: media.width,
+              height: media.height * .8,
+              child: Consumer<ExpensesProvider>(
+                builder: (BuildContext context, ExpensesProvider db, _) {
+                  return ListView(
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    children: db.listOfExpenses
+                        .map(
+                          (e) => HistoryCard(
+                            title: e.title,
+                            amount: e.amount,
+                          ),
+                        )
+                        .toList(),
+                  );
+                },
+              ),
+            ),
           ),
         ],
       ),
