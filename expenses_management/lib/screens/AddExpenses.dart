a63240feba664fd93990d0b4c781e0a3c9f2618d
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2020-05-20 23:23:04 +0200

    bottomNavigationBar was removed

--- expenses_management/lib/screens/AddExpenses.dart
+++ expenses_management/lib/screens/AddExpenses.dart
@@ -32,39 +32,6 @@ class _AddExpensesState extends State<AddExpenses> {
       appBar: AppBar(
         title: Text("Add expenses"),
       ),
-      bottomNavigationBar: BottomAppBar(
-        color: Theme.of(context).primaryColor,
-        shape: const CircularNotchedRectangle(),
-        child: Container(
-          height: media.height * 0.08,
-          child: Padding(
-            padding: EdgeInsets.only(
-              left: media.width * 0.05,
-              right: media.width * 0.05,
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(
-                    Icons.menu,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.search,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           expensesDB.addExpense(
