5650829d372b6e3406851d558ea5e6b0e4568357
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2022-04-18 21:34:45 +0200

    Fix authorization functionality

--- job_app/lib/widgets/authorizationPage/Authorization.dart
+++ job_app/lib/widgets/authorizationPage/Authorization.dart
@@ -11,7 +11,29 @@ class Authorization {
   bool _isLoading = false;
 
   Future<bool> signInWithEmail(email, password, navigatorKey) async {
-    FirebaseUser user;
+
+    User user;
+    /**  NEW CODE **/
+    //this._isLoading = false;
+
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      this._isLoading = false;
+      print(e.toString());
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+    this._isLoading = false;
+    return user != null;
+    /**
     try {
       user = (await _auth.signInWithEmailAndPassword(
         email: email,
@@ -23,6 +45,7 @@ class Authorization {
     }
     this._isLoading = false;
     return user != null;
+    **/
   }
 
   void signOut(context) async {
@@ -32,30 +55,37 @@ class Authorization {
     } catch (e) {
       print(e);
     } finally {
+      User user = FirebaseAuth.instance.currentUser;
+      if(user == null){
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => SingInPage()));
+      }
+      /**
       FirebaseAuth.instance.currentUser().then((firebaseUser) {
         if (firebaseUser == null) {
           Navigator.pushReplacement(
               context, MaterialPageRoute(builder: (context) => SingInPage()));
         }
       });
+      **/
     }
   }
 
   Future<bool> signUpWithEmail(email, password) async {
-    FirebaseUser user;
+    User user;
     try {
       user = (await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
-      )) as FirebaseUser;
+      )) as User;
     } catch (e) {
       print(e.toString());
     }
     return user != null;
   }
 
-  Future<FirebaseUser> getUser() async {
-    return await _auth.currentUser();
+  Future<User> getUser() async {
+    return _auth.currentUser;
   }
 
   bool setToLoad() {
