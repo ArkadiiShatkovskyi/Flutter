7b6022b61dfed6aa7b19aff085a1f92e7ed98972
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2022-04-25 22:55:09 +0200

    Migrate app to new version

--- flutter_and_flare_application/flutter_and_flare_application/android/app/src/main/kotlin/arek/com/flutter_and_flare_application/MainActivity.kt
+++ flutter_and_flare_application/flutter_and_flare_application/android/app/src/main/kotlin/arek/com/flutter_and_flare_application/MainActivity.kt
@@ -1,5 +1,16 @@
 package arek.com.flutter_and_flare_application
 
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
+/**
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
@@ -11,3 +22,4 @@ class MainActivity: FlutterActivity() {
     GeneratedPluginRegistrant.registerWith(this)
   }
 }
+**/
