125bd963cca2593c7621d5b9ca21088ea69c86ff
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2022-04-28 23:07:50 +0200

    FlatButtons were changed to TextButtons

--- job_app/lib/widgets/ConfirmMessage.dart
+++ job_app/lib/widgets/ConfirmMessage.dart
@@ -18,29 +18,42 @@ class ConfirmMessage extends StatelessWidget {
       title: Text(title),
       content: Text(message),
       actions: <Widget>[
-        FlatButton(
-            textColor: Theme.of(context).primaryColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
+        TextButton(
+          style: ButtonStyle(
+            foregroundColor:  MaterialStateProperty.all(Colors.white),
+            backgroundColor:
+                MaterialStateProperty.all(Theme.of(context).primaryColor),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
             ),
-            onPressed: funFirstAnswer,
-            child: const Text(
-              "Yes",
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center,
-            )),
-        FlatButton(
-            color: Theme.of(context).primaryColor,
-            textColor: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
+          ),
+          onPressed: funFirstAnswer,
+          child: const Text(
+            "Yes",
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        TextButton(
+          style: ButtonStyle(
+            foregroundColor:  MaterialStateProperty.all(Colors.white),
+            backgroundColor:
+            MaterialStateProperty.all(Theme.of(context).primaryColor),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
             ),
-            onPressed: funSecondAnswer,
-            child: const Text(
-              "No",
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center,
-            )),
+          ),
+          onPressed: funSecondAnswer,
+          child: const Text(
+            "No",
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          ),
+        ),
       ],
     );
   }
