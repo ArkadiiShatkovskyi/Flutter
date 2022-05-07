3f318504f06009b1f118fada9b1101571fe7d8da
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2022-05-07 15:17:44 +0200

    Prepare widget for route navigation

--- Learning/meals_app/lib/screens/category_meals.dart
+++ Learning/meals_app/lib/screens/category_meals.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({Key key}) : super(key: key);
+  // final String categoryId;
+  // final String categoryTitle;
+  //
+  // const CategoryMealsScreen({Key key, this.categoryId, this.categoryTitle}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+
     return Scaffold(
-      body: Container(
-        child: Text("Test"),
+      body: Center(
+        child: Container(
+          child: Text(categoryTitle),
+        ),
       ),
     );
   }
