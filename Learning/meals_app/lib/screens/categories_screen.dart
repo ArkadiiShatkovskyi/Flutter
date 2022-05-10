a59481c3d178bc4b31f08df64da127b8625fa8c8
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2022-05-10 22:11:11 +0200

    routeName variable was added

--- Learning/meals_app/lib/screens/categories_screen.dart
+++ Learning/meals_app/lib/screens/categories_screen.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/screens/category_meals.dart';
+import 'package:meals_app/screens/category_meals_screen.dart';
 
 import '../dummy_data.dart';
 import '../widgets/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
+  static const routeName = '/categories';
+
   const CategoriesScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Categories'),
+      ),
       body: GridView(
         padding: const EdgeInsets.all(25),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
