6f69fb19104195d5801e1b6e4f58e019aa4ad43d
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2022-05-10 22:14:02 +0200

    Meals dummy data was added

---
+++ Learning/meals_app/lib/screens/category_meals_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+  //
+  // const CategoryMealsScreen({Key key, this.categoryId, this.categoryTitle}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) => meal.categories.contains(categoryId)).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: Center(
+        child: Container(
+          child: ListView.builder(itemBuilder: (ctx, index){
+            return Center(child: Text(categoryMeals[index].title));
+          }, itemCount: categoryMeals.length,),
+        ),
+      ),
+    );
+  }
+}
