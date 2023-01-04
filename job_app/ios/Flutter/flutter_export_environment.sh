af7549cbc5ef6d3f1c59429cbf5d0f421f50b938
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2023-01-04 20:37:01 +0100

    Set up

--- job_app/ios/Flutter/flutter_export_environment.sh
+++ job_app/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\white\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\white\Desktop\Dev\Flutter\job_app"
+export "FLUTTER_APPLICATION_PATH=C:\Users\white\Desktop\Dev\Course\Flutter_github\Flutter\job_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
