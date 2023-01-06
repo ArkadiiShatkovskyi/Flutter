e7cf871c44130126c12e334bfdf91124f378785a
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2023-01-06 15:59:41 +0100

    Creating Matrix of the gmae

--- SnakeGame/snake_game/lib/screens/game_screen.dart
+++ SnakeGame/snake_game/lib/screens/game_screen.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatefulWidget {
-  final int matrixWidth;
-  final int matrixHeight;
+  final int rows;
+  final int columns;
+  var _matrix;
 
-  const GameScreen(
-      {Key? key, required this.matrixWidth, required this.matrixHeight})
-      : super(key: key);
+  GameScreen({Key? key, required this.rows, required this.columns})
+      : super(key: key) {
+    _matrix = List.generate(
+        rows, (i) => List.generate(columns, (j) => 0, growable: false),
+        growable: false);
+
+    _matrix.forEach((row) {
+      //print("Row: " + row.toString());
+      print(row);
+    });
+  }
 
   @override
   State<StatefulWidget> createState() => _GameScreen_State();
@@ -17,9 +26,27 @@ class _GameScreen_State extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: const Text("Game Screen"),
-      ),
+      body: _getMatrixWidget(),
+    );
+  }
+
+  Widget _getMatrixWidget() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: widget._matrix.map<Widget>(
+        (row) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: row.map<Widget>(
+              (column) {
+                return Container(
+                  child: Text(column.toString()),
+                );
+              },
+            ).toList(),
+          );
+        },
+      ).toList(),
     );
   }
 }
