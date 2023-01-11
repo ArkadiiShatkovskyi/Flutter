7814462fb5a4c4ed37e802b3afffd5776cdc433a
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2023-01-11 23:03:45 +0100

    Preparing dynamics functions for moves right and left

--- SnakeGame/snake_game/lib/models/game_movement.dart
+++ SnakeGame/snake_game/lib/models/game_movement.dart
@@ -1,47 +1,42 @@
 import 'package:snake_game/models/matrix_element.dart';
 
-int moveSnakeRight(var matrix, int _strIndRow, int _strIndCol, int columns) {
-  //print("Value 1 3: " + (_strIndCol + 1).toString());
-  if (_strIndCol == 1) {
-    if (matrix[_strIndRow][columns - 1].value == 1) {
-      matrix[_strIndRow][columns - 1].value = 0;
+List<List<int>> moveSnakeRight(int columns, List<MatrixElement> list, var matrix) {
+  for(MatrixElement m in list){
+    if(m.column + 1 > columns - 1){
+      m.column = 0;
+    } else{
+      m.column += 1;
     }
-    matrix[_strIndRow][_strIndCol - 1].value = 0;
-
-    matrix[_strIndRow][_strIndCol].value = 1;
-    matrix[_strIndRow][_strIndCol + 1].value = 1;
-  } else if (_strIndCol + 1 < columns) {
-    if (_strIndCol - 1 >= 0) {
-      matrix[_strIndRow][_strIndCol - 1].value = 0;
-    }
-    matrix[_strIndRow][_strIndCol].value = 1;
-    matrix[_strIndRow][_strIndCol + 1].value = 1;
-  } else if (_strIndCol + 1 >= columns) {
-    if (_strIndCol - 1 >= 0 && _strIndCol - 1 < columns) {
-      matrix[_strIndRow][_strIndCol - 1].value = 0;
-    }
-
-    _strIndCol = 0;
-    matrix[_strIndRow][_strIndCol].value = 1;
+    matrix[m.row][m.column] = 1;
   }
-  _strIndCol = _strIndCol + 1;
+  int col = list[list.length - 1].column;
+  int row = list[list.length - 1].row;
 
-  return _strIndCol;
+  col = col - 1 < 0 ? columns - 1 : col - 1;
+  matrix[row][col] = 0;
+
+  return matrix;
 }
 
-int moveSnakeLeft(var matrix, int _strIndRow, int _strIndCol, int columns) {
-  //print("Value 1 3: " + (_strIndCol + 1).toString());
-
-  //moveRight
-  if(_strIndCol == 0){
-
+List<List<int>> moveSnakeLeft(int columns, List<MatrixElement> list, var matrix) {
+  for(MatrixElement m in list){
+    if(m.column - 1 < 0){
+      m.column = columns - 1;
+    } else{
+      m.column -= 1;
+    }
+    matrix[m.row][m.column] = 1;
   }
-  _strIndCol = _strIndCol + 1;
+  int col = list[list.length - 1].column;
+  int row = list[list.length - 1].row;
 
-  return _strIndCol;
+  col = col + 1 > columns - 1 ? 0 : col + 1;
+  matrix[row][col] = 0;
+
+  return matrix;
 }
 
-List<MatrixElement> getSnakeElements(var matrix){
+List<MatrixElement> getSnakeElements(var matrix) {
   List<MatrixElement> list = [];
   int ind = 0;
   for(int i = 0; i < matrix[i].size; i++){
