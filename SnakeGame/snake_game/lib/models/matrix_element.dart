921948f40a790ecf575355e9032709155917c5b3
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2023-01-16 22:26:36 +0100

    Adding direction to Matrix Element model

--- SnakeGame/snake_game/lib/models/matrix_element.dart
+++ SnakeGame/snake_game/lib/models/matrix_element.dart
@@ -2,6 +2,7 @@ class MatrixElement{
   int row;
   int column;
   int value;
+  int direction;
 
-  MatrixElement({required this.row, required this.column, required this.value});
+  MatrixElement({required this.row, required this.column, required this.value, required this.direction});
 }
